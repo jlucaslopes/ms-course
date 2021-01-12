1ec726282a1dcf577d92ddbdc21c6b70fa9b96e8
jlucaslopes <jlucasslhp@gmail.com>
2021-01-12 12:00:51 -0300

    Zuul Timeout

--- hr-worker/src/main/java/com/jlopes/hrworker/controllers/WorkerResources.java
+++ hr-worker/src/main/java/com/jlopes/hrworker/controllers/WorkerResources.java
@@ -35,12 +35,12 @@ public class WorkerResources {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
 
-        /*
+
         try {
             Thread.sleep(3000L);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }*/
+        }
 
         logger.info("PORT = "+env.getProperty("local.server.port"));
 
