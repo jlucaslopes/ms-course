5c140a93908e4835708658cb33e50094f7d4c661
jlucaslopes <jlucasslhp@gmail.com>
2021-01-29 10:05:25 -0300

    add Dockerfile in some projects

--- hr-worker/src/main/java/com/jlopes/hrworker/controllers/WorkerResources.java
+++ hr-worker/src/main/java/com/jlopes/hrworker/controllers/WorkerResources.java
@@ -23,8 +23,7 @@ public class WorkerResources {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResources.class);
 
-    @Value("${test.config}")
-    private String testConfig;
+   // @Value("${test.config}")//private String testConfig;
 
     @Autowired
     private Environment env;
@@ -34,7 +33,7 @@ public class WorkerResources {
 
     @GetMapping(value = "/configs")
     public ResponseEntity<Void> getConfigs() {
-      logger.info("CONFIG = "+testConfig);
+     // logger.info("CONFIG = "+testConfig);
       return ResponseEntity.noContent().build();
     }
 
